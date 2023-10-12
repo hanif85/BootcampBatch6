5dc20b0c377a8a671b72f49b33028e61c7ff738d
hanif85 <hanifmohammed85@gmail.com>
2023-10-12 16:57:34 +0700

    Learn about inherritance Overriding, Abstraction, Interface

---
+++ Day4 Interface/Program.cs
@@ -0,0 +1,90 @@
+﻿
+// Interface
+using System.Runtime.InteropServices.Marshalling;
+
+interface IAnimal 
+{
+  void animalSound(); // interface method (does not have a body)
+  void isTailLong(bool tail);
+}
+interface IMammalia
+{
+	void drinkMilk();
+	void isbreathWithLung();
+}
+
+// Cat "implements" the IAnimal interface
+class Cat : IAnimal, IMammalia
+{
+  public void animalSound() 
+  {
+	// The body of animalSound() is provided here
+	Console.WriteLine("The Cat says: miauw miauw");
+  }
+
+	public void drinkMilk()
+	{
+		Console.WriteLine("Cat drink milk");
+	}
+
+	public void isbreathWithLung()
+	{
+		Console.WriteLine("Yes, I have Lung");
+	}
+
+	public void isTailLong(bool tail)
+  {
+	if (tail)
+	{
+		Console.WriteLine("The tail is long");
+	}else{
+		Console.WriteLine("the tail is short");
+	}
+  	
+  }
+}
+class Goat : IAnimal, IMammalia
+{
+  public void animalSound() 
+  {
+	// The body of animalSound() is provided here
+	Console.WriteLine("The Goat says: mbeeek mbeeek");
+  }
+
+	public void drinkMilk()
+	{
+		Console.WriteLine("Goat drink milk");
+	}
+
+	public void isbreathWithLung()
+	{
+		Console.WriteLine("Yes, I have Lung");
+	}
+
+	public void isTailLong(bool tail)
+  {
+	if (tail)
+	{
+		Console.WriteLine("The tail is long");
+	}else{
+		Console.WriteLine("the tail is short");
+	}
+  	
+  }
+}
+class Program 
+{
+  static void Main(string[] args) 
+  {
+	Cat myCat = new Cat();  // Create a Pig object
+	myCat.animalSound();
+	myCat.isTailLong(true);
+	myCat.drinkMilk();
+	myCat.isbreathWithLung();
+	Goat mygoat = new Goat();
+	mygoat.animalSound();
+	mygoat.isTailLong(false);
+	mygoat.drinkMilk();
+	mygoat.isbreathWithLung();
+  }
+}
---
+++ Day4 inheritanceN_Overriding/Program.cs
@@ -0,0 +1,83 @@
+﻿class Program{
+//Inheritance OK and Overriding and Method Hiding
+static void Main() {
+	Engine general = new DieselEngine();
+	//Engine e = new Engine();
+	//DieselEngine diesel = new DieselEngine();
+	//e = diesel;
+
+	DieselEngine diesel = new();
+	ElectricEngine electric = new();
+	HidrogenEngine hidrogenEngine = new();
+	hidrogenEngine.Emission();
+	diesel.Emission();
+	
+	
+	Car car = new Car(diesel);
+	car.EngineStart();
+	
+}
+}
+class Car
+{
+	private Engine _engine;
+	public Car(Engine e)
+	{
+		_engine = e;
+	}
+	public void EngineStart()
+	{
+		_engine.Run();
+		_engine.WarmUp();
+	}
+
+}
+class Engine {
+	public virtual void Run() {
+		Console.WriteLine("Engine Run");
+	}
+	public virtual void WarmUp() {
+	}
+	public virtual void Emission()
+	{
+		Console.WriteLine("Check emission");
+	}
+}
+class DieselEngine : Engine
+{
+	public override void Run()
+	{
+		Console.WriteLine("Diesel Engine Run");
+	}
+	public override void WarmUp() {
+		Console.WriteLine("Diesel Engine Warm Up");
+	}
+	public override void Emission()
+	{
+		Console.WriteLine("Diesel  is low emission");
+	}
+}
+class ElectricEngine : Engine
+{
+	public override void Run()
+	{
+		Console.WriteLine("Electric Engine Run");
+	}
+	public override void Emission()
+	{
+		Console.WriteLine("Hidrogen is low emission");
+	}
+}
+
+class HidrogenEngine : Engine
+{
+	public override void Run()
+	{
+		Console.WriteLine("Hidrogen Engine Run");
+		
+	}
+	public override void Emission()
+	{
+		Console.WriteLine("Hidrogen is low emission");
+	}
+}
