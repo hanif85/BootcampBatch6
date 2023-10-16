55f822313d43fb67c258ab42c6b67bcb904b766c
hanif85 <hanifmohammed85@gmail.com>
2023-10-16 17:01:04 +0700

    Day6 generic static etc

---
+++ Day6 ConstIsStaticGlobal/Program.cs
@@ -0,0 +1,14 @@
+﻿//Constant is static global
+public class Program
+{
+        static void Main()
+        {
+            MyMath math = new MyMath();
+            //math.phi.Dump(); cant call static variable from instance
+            Console.WriteLine(MyMath.phi);
+            
+        }
+}
+class MyMath {
+	public const float phi = 3.14f; //Const is static
+}
